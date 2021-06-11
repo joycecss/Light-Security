af68bc78b5fab95f8a7afc56a4f738e8ac6e227d
joyce cristyna <joohcristyna@gmail.com>
2021-06-10 21:40:53 -0300

    pfvr

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-process.env.NODE_ENV = 'dev'; // ALTERE AQUI seu ambiente para 'dev' ou 'production'
+process.env.NODE_ENV = 'dev'; // altere para 'production' ou 'dev'
 
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//importanto as funcionalidades do usuario (armazena valores)
 var indexRouter = require('./routes/index');
-var apiSensores = require('./routes/api-sensores');
+var usuariosRouter = require('./routes/usuarios'); 
+var leiturasRouter = require('./routes/leituras');
 
 var app = express();
 
-app.use(logger('dev')); // coloque aqui também, o mesmo valor da linha 1
+// use essas funcionalidades
+app.use(logger('dev')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api', apiSensores);
+app.use('/usuarios', usuariosRouter);
+app.use('/leituras', leiturasRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).send("<h1>Hello World!</h1> Para inserir dados usando a API, vá até <a href=\"http://localhost:9001/api/sendData\">http://localhost:9001/api/sendData</a>");
-  //res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
